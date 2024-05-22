2e52d2aa7d34ba160b5eadc89e3782d36f5069e6
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-05-22 22:16:56 +0600

    iventor quantity sucecssfully counted when order by user

--- src/app/modules/order/order.model.ts
+++ src/app/modules/order/order.model.ts
@@ -1,5 +1,6 @@
 import { Schema, model } from "mongoose";
 import { IOrder } from "./order.interface";
+import { ProductModel } from "../product/product.model";
 
 const orderSchema = new Schema<IOrder>({
   email: {
@@ -18,6 +19,46 @@ const orderSchema = new Schema<IOrder>({
     type: Number,
     required: true,
   },
+});
+
+orderSchema.pre("save", async function (next) {
+  const result = await ProductModel.findById(this.productId);
+
+  if (!result) {
+    throw new Error(`Product does not exist for productId ${this.productId}`);
+  }
+
+  // checking if the requested quantity is greater then the product quantity
+  const {
+    inventory: { quantity },
+  }: any = await ProductModel.findById(this.productId);
+
+  if (quantity < this.quantity) {
+    throw new Error(
+      `Insufficient quantity available for this inventory. Quantity: ${this.quantity}`
+    );
+  }
+
+  //  reduce the product quantity when ordered
+  const updatedProduct = await ProductModel.findByIdAndUpdate(
+    this.productId,
+    {
+      $inc: {
+        "inventory.quantity": -this.quantity,
+      },
+    },
+    { new: true }
+  );
+
+  //update the inStock if quantity is 0
+  if (updatedProduct?.inventory.quantity === 0) {
+    await ProductModel.findByIdAndUpdate(this.productId, {
+      $set: {
+        "inventory.inStock": false,
+      },
+    });
+  }
+  next();
 });
 
 export const OrderModel = model<IOrder>("Order", orderSchema);
